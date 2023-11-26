396daad835160d674fe34683ae91d199b410237b
tr Wang <tr wang@macbook-pro-5.local>
2023-11-26 11:59:54 +0000

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Patient pat1 = new Patient("John Molecules",true,true);	// Print to screen
         Patient pat2 = new Patient("Jill Biomedenhall",false,true);
         pat1.getInfo();
+<<<<<<< HEAD
+=======
+        pat2.getInfo();
+>>>>>>> 4a4f5d5 (Initial commit)
 
 
     }
